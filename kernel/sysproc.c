e88b5b6611f6ee6a2e8c65c6ee5e3663a12c062b
PrinceWYS <Wys2114237282@163.com>
2025-02-28 15:00:22 +0800

    add alarm

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -96,3 +96,28 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64 sys_sigalarm(void) {
+  int ticks = 0;
+  struct proc* p = myproc();
+  uint64 handler;
+
+  if (argint(0, &ticks) < 0) {
+    return -1;
+  }
+  if (argaddr(1, &handler) < 0) {
+    return -1;
+  }
+
+  p->alarm_interval = ticks;
+  p->alarm_handler = (void(*)())handler;
+
+  return 0;
+}
+
+uint64 sys_sigreturn(void) {
+  struct proc* p = myproc();
+  *p->trapframe = *p->alarmframe;
+  p->alarm_stat = 0;
+  return 0;
+}
